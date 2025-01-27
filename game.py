2732ad411f1c2e17e6aa4e29931c3e4e9a067650
yaroslavasrf <ya.sarafanova@gmail.com>
2025-01-27 22:49:23 +0300

    add 7
    add 7

--- game.py
+++ game.py
@@ -24,10 +24,30 @@ class Game:
             if event.type == pygame.QUIT:
                 self.running = False
 
+    def draw_grid(self):
+        """Рисует сетку поля."""
+
+        # Рисуем сетку на экране
+        for y in range(ROWS):
+            for x in range(COLUMNS):
+                rect = pygame.Rect(x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE)
+                pygame.draw.rect(self.screen, GREY, rect, 1)
+    def render(self):
+        """Отображает всю информацию на экране."""
+
+        # Заполнение экрана чёрным цветом
+        self.screen.fill(BLACK)
+
+        # Рисуем сетку, зафиксированные блоки и текущую падающую фигуру
+        self.draw_grid()
+
+        # Обновление экрана
+        pygame.display.flip()
 
     def run(self):
         """Запуск игрового цикла."""
 
         while self.running:
             self.handle_events()  # Обработка событий
+            self.render()  # Отображение на экране
             self.clock.tick(60)  # Ограничение FPS (60 кадров в секунду)
--- settings.py
+++ settings.py
@@ -4,5 +4,17 @@ SCREEN_WIDTH = 300
 SCREEN_HEIGHT = 600
 # Размер ячейки сетки
 GRID_SIZE = 30
+# Количество столбцов в сетке
+COLUMNS = SCREEN_WIDTH // GRID_SIZE
+# Количество строк в сетке
+ROWS = SCREEN_HEIGHT // GRID_SIZE
 # Частота обновления кадров
 FPS = 60
+
+
+
+# ЦВЕТА
+# Чёрный цвет
+BLACK = (0, 0, 0)
+# Серый цвет для рисования сетки
+GREY = (128, 128, 128)
